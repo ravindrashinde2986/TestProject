b7df6a64f99560e51945aadb2b99012ee2ba3f4b
Ravindra Shinde <ravindra.shinde@siemens.com>
2023-07-01 17:37:36 +0530

    Minor changes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
 from .router import users, posts, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def health_check():
+    return "Hello World"
 
 # my_posts = [
 #     {"id": 1, "title": "list of tech books", "content": "Check out these books"},
